b16322240d5b0f1b46a950fe53ae72c6dc9a1a1d
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-05-28 11:05:42 -0500

    adding to cloud repo

--- scripts.js
+++ scripts.js
@@ -1,2 +1,44 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+function init () {
+
+    const takeoffButton = document.getElementById("takeoff");
+    const landingButton = document.getElementById("landing");
+    const abortMissionButton = document.getElementById("missionAbort");
+
+        function takeoffConfirm () {
+            let confirm = window.confirm("Confirm that the shuttle is ready for takeoff.");
+            if (confirm) {
+                flightStatus.innerHTML = "Shuttle in flight";
+                shuttleBackground.style.backgroundColor = "blue";
+                spaceShuttleHeight.innerHTML += 10000;
+            }
+
+        }
+        takeoff.addEventListener("click", takeoffConfirm);
+
+        function land () {
+            window.alert("The shuttle is landing. Landing gear engaged.");
+            flightStatus.innerHTML = "The shuttle has landed.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = 0;
+
+        }
+        landing.addEventListener("click", land);
+
+        function abort () {
+            let confirm = window.confirm("Confirm that you want to abort the mission.");
+            if (confirm) {
+                flightStatus.innerHTML = "Mission aborted.";
+                shuttleBackground.style.backgroundColor = "green";
+                spaceShuttleHeight.innerHTML = 0;
+            }
+        }
+        missionAbort.addEventListener("click", abort);
+
+
+
+}
+
+window.addEventListener("load", init);
